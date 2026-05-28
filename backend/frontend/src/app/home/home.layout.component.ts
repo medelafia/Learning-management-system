2da04a698114deb3c83e912afab184af35574146
MOHAMED EL AFIA <mohamedelafia723@gmail.com>
2026-05-28 19:03:41 +0100

    Before Angular 17 upgrade

---
+++ backend/frontend/src/app/home/home.layout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-layout-home',
+  templateUrl: './home.layout.component.html',
+  styleUrls: [] ,
+})
+export class HomeLayoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
