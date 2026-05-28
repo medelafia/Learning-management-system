511158f88fa1994429394c0440203f3a1f4a5c71
MOHAMED EL AFIA <mohamedelafia723@gmail.com>
2026-05-28 19:26:23 +0100

    Before Angular 17 upgrade

--- backend/frontend/src/app/core/guards/auth.guard.ts
+++ backend/frontend/src/app/core/guards/auth.guard.ts
@@ -1,18 +1,12 @@
 import { Injectable } from '@angular/core';
-import {
-  ActivatedRouteSnapshot,
-  CanActivate,
-  Router,
-  RouterStateSnapshot,
-  UrlTree,
-} from '@angular/router';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { AuthService } from '@core/services/auth.service';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard  {
   constructor(private authService: AuthService, private router: Router) {}
 
   canActivate(
--- backend/frontend/src/app/core/guards/logged-in-auth.guard.ts
+++ backend/frontend/src/app/core/guards/logged-in-auth.guard.ts
@@ -1,18 +1,12 @@
 import { Injectable } from '@angular/core';
-import {
-  ActivatedRouteSnapshot,
-  CanActivate,
-  Router,
-  RouterStateSnapshot,
-  UrlTree,
-} from '@angular/router';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { AuthService } from '@core/services/auth.service';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
-export class LoggedInAuthGuard implements CanActivate {
+export class LoggedInAuthGuard  {
   constructor(private authService: AuthService, private router: Router) {}
 
   canActivate(
