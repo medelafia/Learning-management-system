d768a89a248b4f59d2f591704492a8e35975ce7d
MOHAMED EL AFIA <mohamedelafia723@gmail.com>
2026-06-01 22:03:02 +0100

    pre deployment configuration

--- deploy.sh
+++ deploy.sh
@@ -1,43 +1,44 @@
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update 
 
-helm install database-auth bitnami/mysql --namespace faculty -f ./charts/database-auth/values.yaml \
-    --set auth.rootPassword=./env-variables/.env.database_auth:MYSQL_ROOT_PASSWORD \
-    --set auth.database=./env-variables/.env.database_auth:MYSQL_DATABASE \
-    --set auth.username=./env-variables/.env.database_auth:MYSQL_USER \
-    --set auth.password=./env-variables/.env.database_auth:MYSQL_PASSWORD 
+kubectl create secret generic database-auth-secret --from-env-file=./env-variables/.env.database_auth -n faculty
+kubectl create secret generic database-exam-secret --from-env-file=./env-variables/.env.database_exam -n faculty
+kubectl create secret generic database-subject-secret --from-env-file=./env-variables/.env.database_subject -n faculty
+kubectl create secret generic database-faculty-secret --from-env-file=./env-variables/.env.database_faculty -n faculty
 
-helm install database-exam bitnami/mysql --namespace faculty -f ./charts/database-exam/values.yaml\
-    --set auth.rootPassword=./env-variables/.env.database_exam:MYSQL_ROOT_PASSWORD \
-    --set auth.database=./env-variables/.env.database_exam:MYSQL_DATABASE \
-    --set auth.username=./env-variables/.env.database_exam:MYSQL_USER \
-    --set auth.password=./env-variables/.env.database_exam:MYSQL_PASSWORD 
+helm upgrade --install database-auth bitnami/mysql -n faculty -f ./charts/database-auth/values.yaml \
+    --set auth.existingSecret=database-auth-secret
 
-helm install database-subject bitnami/mysql --namespace faculty -f ./charts/database-subject/values.yaml \
-    --set auth.rootPassword=./env-variables/.env.database_subject:MYSQL_ROOT_PASSWORD \
-    --set auth.database=./env-variables/.env.database_subject:MYSQL_DATABASE \
-    --set auth.username=./env-variables/.env.database_subject:MYSQL_USER \
-    --set auth.password=./env-variables/.env.database_subject:MYSQL_PASSWORD 
+helm upgrade --install database-exam bitnami/mysql -n faculty -f ./charts/database-exam/values.yaml\
+    --set auth.existingSecret=database-exam-secret
 
-helm install database-faculty bitnami/mysql --namespace faculty -f ./charts/database-faculty/values.yaml\
-    --set auth.rootPassword=./env-variables/.env.faculty:MYSQL_ROOT_PASSWORD \
-    --set auth.database=./env-variables/.env.faculty:MYSQL_DATABASE \
-    --set auth.username=./env-variables/.env.faculty:MYSQL_USER \
-    --set auth.password=./env-variables/.env.faculty:MYSQL_PASSWORD 
+helm upgrade --install database-subject bitnami/mysql -n faculty -f ./charts/database-subject/values.yaml \
+    --set auth.existingSecret=database-subject-secret
+
+helm upgrade --install database-faculty bitnami/mysql -n faculty -f ./charts/database-faculty/values.yaml\
+    --set auth.existingSecret=database-faculty-secret
 
 
-kubectl create secret generic auth-service-secret --from-file=./env-variables/.env.auth_service -n faculty
-kubectl create secret generic api-gateway-secret --from-file=./env-variables/.env.api_gateway -n faculty
-kubectl create secret generic discovery-service-secret --from-file=./env-variables/.env.discovery_service -n faculty
-kubectl create secret generic config-server-secret --from-file=./env-variables/.env.config_server -n faculty
-kubectl create secret generic exam-service-secret --from-file=./env-variables/.env.exam_service -n faculty
-kubectl create secret generic subject-service-secret --from-file=./env-variables/.env.subject_service -n faculty
-kubectl create secret generic faculty-service-secret --from-file=./env-variables/.env.faculty_service -n faculty
+kubectl create secret generic auth-service-secret --from-env-file=./env-variables/.env.auth_service -n faculty
+kubectl create secret generic api-gateway-secret --from-env-file=./env-variables/.env.api_gateway -n faculty
+kubectl create secret generic discovery-service-secret --from-env-file=./env-variables/.env.discovery_service -n faculty
+kubectl create secret generic config-server-secret --from-env-file=./env-variables/.env.config_server -n faculty
+kubectl create secret generic exam-service-secret --from-env-file=./env-variables/.env.exam_service -n faculty
+kubectl create secret generic subject-service-secret --from-env-file=./env-variables/.env.subject_service -n faculty
+kubectl create secret generic faculty-service-secret --from-env-file=./env-variables/.env.faculty_service -n faculty
+
+eval $(minikube docker-env)
+docker build -t config-service-container:latest ./config-server
+docker build -t api-gateway-container:latest ./api-gateway
+docker build -t auth-service-container:latest ./auth-service
+docker build -t exam-service-container:latest ./exam-service
+docker build -t faculty-service-container:latest ./faculty-service
+docker build -t subject-service-container:latest ./subject-service
 
 helm install api-gateway ./charts/api-gateway-chart -n faculty
-helm install auth-service-chart ./charts/auth-service-chart -n faculty
-helm install config-service-chart ./charts/config-service-chart -n faculty
-helm install discovery-service-chart ./charts/discovery-service-chart -n faculty
-helm install exam-service-chart ./charts/exam-service-chart -n faculty
-helm install faculty-service-chart ./charts/faculty-service-chart -n faculty
-helm install subject-service-chart ./charts/subject-service-chart -n faculty
+helm install auth-service ./charts/auth-service-chart -n faculty
+helm install config-service ./charts/config-service-chart -n faculty
+helm install discovery-service ./charts/discovery-service-chart -n faculty
+helm install exam-service ./charts/exam-service-chart -n faculty
+helm install faculty-service ./charts/faculty-service-chart -n faculty
+helm install subject-service ./charts/subject-service-chart -n faculty
