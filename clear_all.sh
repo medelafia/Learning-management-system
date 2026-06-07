a7abdb7d75434bd8d6612cea3f92d4125eb364c1
MOHAMED EL AFIA <mohamedelafia723@gmail.com>
2026-06-07 01:29:37 +0100

    deployment on kubernetes

--- clear_all.sh
+++ clear_all.sh
@@ -1,18 +1,34 @@
+echo "Stage I : Clearing all helm charts on faculty-app namespace..."
+echo "##############################################################"
 helm list -n faculty-app --short | xargs -L1 helm uninstall -n faculty-app
+
+echo "Stage II : Deleting databases and volumes.."
+echo "##############################################################" 
 DATABASES=("auth" "exam" "subject" "faculty")
 for database_name in "${DATABASES[@]}"; do
-    echo "Deleting database-${database_name}-secret"  
+    echo "##############################################################"
+    echo "❌🔐 Deleting database-${database_name}-secret"  
     kubectl delete secret "database-${database_name}-secret" -n faculty-app
-    echo "Deleting volume for ${database_name} database" 
+    echo "❌💾 Deleting volumes for ${database_name} database" 
     kubectl delete -f "./charts/database-${database_name}/Storage.yaml"
-    echo "Deleting deployment and service for ${database_name} database" 
+    echo "❌☸️ Deleting deployment and service for ${database_name} database" 
     kubectl delete -f "./charts/database-${database_name}/DeploymentService.yaml"
 done
+echo "✅✅ All databases deleted!"
 
-
+echo "Stage III : Deleting microservices secrets..."
+echo "##############################################################"
 microservices=("config-server" "discovery-server" "auth-service" "api-gateway" "exam-service" "subject-service" "faculty-service")
 for microservice in "${microservices[@]}"; do
     kubectl delete secret "${microservice}-secret" -n faculty-app
 done
+echo "✅✅ All microservices secrets deleted!"
 
+
+echo "Stage VI : Deleting frontend configmap..."
+echo "##############################################################"
 kubectl delete configmap frontend-configmap -n faculty-app
+echo "✅✅ All frontend configmap deleted!"
+
+
+echo "✅✅✅✅ Finish!"
--- deploy.sh
+++ deploy.sh
@@ -1,50 +1,83 @@
+####################################################################################################
+####    Copy Right 2026 (C) Mohamed EL AFIA                                                        #
+####    script : setup_all.sh                                                                      #
+####    description : This script is responsible for setup the entire application and their        #
+####                  dependancies on kubernetes cluster (databases , microservies, ...)           #             
+####################################################################################################
 
 
+echo " --------------------------------------------------------------"
+echo "|                Stage I: Cheking namespace                    |"
+echo " --------------------------------------------------------------"
 
-
+if kubectl get namespace faculty-app >/dev/null 2>&1; then
+    echo "🎓✅ faculty-app namespace already exists"
+else    
+    echo "🎓 Creating faculty-app namespace..."
+    kubectl create namespace faculty-app
+    echo "🎓✅ faculty-app namespace created..."
+fi
 
 DATABASES=("auth" "exam" "subject" "faculty")
 echo " --------------------------------------------------------------"
-echo "|                Stage I: Deploying databases                  |"
+echo "|                Stage II: Deploying databases                 |"
 echo " --------------------------------------------------------------"
-for database_name in "${DATABASES[@]}"; do
-
-    echo "🦈🔐 Creating database-${database_name}-secret..."  
-    kubectl create secret generic "database-${database_name}-secret" --from-env-file="./env-variables/.env.database_${database_name}" -n faculty-app
+for database_name in "${DATABASES[@]}"; do 
+    if kubectl get secret "database-${database_name}-secret" >/dev/null 2>&1; then
+        echo "🎓✅ database-${database_name}-secret secret already exists"
+    else    
+       echo "🦈🔐 Creating database-${database_name}-secret..."  
+        kubectl create secret generic "database-${database_name}-secret" --from-env-file="./env-variables/.env.database_${database_name}" -n faculty-app
+    fi
     echo "🦈💾 Creating volume for ${database_name} database..." 
     kubectl apply -f "./charts/database-${database_name}/Storage.yaml"
     echo "🦈⛏️⚒️ Creating deployment and exposing service for ${database} database..." 
     kubectl apply -f "./charts/database-${database_name}/DeploymentService.yaml"
 done
-
+echo "✅✅ All databases are ready now!"
 
 microservices=("config-server" "discovery-server" "auth-service" "api-gateway" "exam-service" "subject-service" "faculty-service")
 echo " --------------------------------------------------------------"
-echo "|              Stage I: Deploying microservices                |"
+echo "|            Stage III: Deploying microservices                |"
 echo " --------------------------------------------------------------"
 for microservice in "${microservices[@]}"; do
-    echo "⚒️ Building image : ${microservice}"  
+    echo "###############################################################"
+    echo "⚒️💿 Building ${microservice} image :"  
     docker build -t "${microservice}:latest" "./${microservice}"
+    echo "🔗💿 Tagging image : ${microservice}" 
     docker tag "${microservice}:latest" "mohamedelafia/${microservice}:latest"
-    echo "Pushing image : ${microservice}"  
+    echo "📌💿 Pushing ${microservice} image "
     docker push "mohamedelafia/${microservice}:latest"
-    echo "Creating ${microservice}-secret"  
-    kubectl create secret generic "${microservice}-secret" --from-env-file="./env-variables/.env.${microservice}" -n faculty-app
-
+    if kubectl get secret "${microservice}-secret" >/dev/null 2>&1; then
+        echo "🎓✅ ${microservice}-secret already exists"
+    else    
+        echo "🔐💿 Creating ${microservice}-secret" 
+        kubectl create secret generic "${microservice}-secret" --from-env-file="./env-variables/.env.${microservice}" -n faculty-app
+    fi
+    echo "☸️🚀 Deploying ${microservice} on the cluster"  
     helm upgrade --install "${microservice}" "./charts/${microservice}-chart" -n faculty-app
 done
+echo "✅✅ All microservices are ready now!"
 
 
 ## preparing frontend
-
+echo " --------------------------------------------------------------"
+echo "|             Stage IV: Deploying frontend                     |"
+echo " --------------------------------------------------------------"
 IP_ADDRESS=$(kubectl get svc api-gateway-service -n faculty-app -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
-echo "Building frontend image..." 
-docker build -t faculty-app-frontend:latest ./frontend
-echo "Tagging frontend image..." 
-docker tag faculty-app-frontend:latest mohamedelafia/faculty-app-frontend:latest
-echo "Pushing frontend image..." 
-docker push mohamedelafia/faculty-app-frontend:latest
-echo "Image ready!"
 
+echo "✅ Api gateway IP address resolved : $IP_ADDRESS"
+
+echo "⚒️💿 Building frontend image..." 
+docker build -t faculty-app-frontend:latest ./frontend
+echo "🔗💿 Tagging frontend image..." 
+docker tag faculty-app-frontend:latest mohamedelafia/faculty-app-frontend:latest
+echo "📌💿 Pushing frontend image..." 
+docker push mohamedelafia/faculty-app-frontend:latest
+echo "⚙️💿 Creating configmap for frontend..."  
 kubectl create configmap frontend-configmap --from-literal=API_ENDPOINT="${IP_ADDRESS}" -n faculty-app
+echo "☸️🚀 Deploying frontend on the cluster"  
 helm upgrade --install faculty-app-frontend "./charts/frontend-chart" -n faculty-app
+
+echo "✅✅ Frontend is ready now!"
+echo "✅✅ application is ready now!"
